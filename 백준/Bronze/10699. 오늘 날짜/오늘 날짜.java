915b998324bb34790cd1c7be44e717c48c78df23
jolbzz <ksh120377@gmail.com>
2024-11-24 19:43:12 +0900

    [Bronze V] Title: 오늘 날짜, Time: 164 ms, Memory: 17352 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.time.LocalDate;
+
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) 
+    {
+        LocalDate date = LocalDate.now();
+        System.out.println(date);
+    }
+}
