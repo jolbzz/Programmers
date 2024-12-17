a64619cfff1dd323a2a6e005379872731d380024
jolbzz <ksh120377@gmail.com>
2024-12-17 20:28:51 +0900

    [level 2] Title: 전화번호 목록, Time: 310.25 ms, Memory: 100 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution 
+{
+    public boolean solution(String[] phone_book) 
+    {
+        boolean answer = true;
+        Arrays.sort(phone_book);
+        
+        for(int i = 1; i < phone_book.length; i++)
+        {
+            if(phone_book[i].startsWith(phone_book[i - 1]) == true)
+            {
+                return false;    
+            }
+        }
+        
+        return answer;
+    }
+}
