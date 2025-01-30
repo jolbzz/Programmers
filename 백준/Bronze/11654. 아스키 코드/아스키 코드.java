9258f99b658da17d4b2e9ee2bfe7b187510983eb
jolbzz <ksh120377@gmail.com>
2025-01-30 21:11:07 +0900

    [Bronze V] Title: 아스키 코드, Time: 104 ms, Memory: 14216 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char num = (char)br.read();
+        System.out.println((int)num);
+    }
+}
